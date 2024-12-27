6bca315b2b6061e8c5cf35a97b47c4d8f0dee34f
Tomas Jay <tomasj21481@gmail.com>
2024-12-27 01:14:04 -0500

    fixed nav bar

--- _includes/js/script.js
+++ _includes/js/script.js
@@ -1,7 +1,44 @@
+console.log("Document ready!");
+
 $(document).ready(function(){
+  
+  console.log("Document ready!");
+
   bindProjectNavArrows();
   fadePageIn();
-});
+
+  // Sticky header logic
+  let lastScrollTop = 0;         // Tracks the previous scroll position
+  const scrollThreshold = 10;    // Number of pixels before the header becomes fixed
+  
+  $(window).on('scroll', function() {
+    const currentScroll = $(this).scrollTop();
+
+    // Check if the user has scrolled at least scrollThreshold pixels
+    if (currentScroll >= scrollThreshold) {
+      // Make header sticky
+      $('.site-header').addClass('sticky');
+      
+      // If user scrolls down quickly (for example, more than 5px at once),
+      // add a quick fade to smooth the transition.
+      if (currentScroll - lastScrollTop > 5) {
+        $('.site-header').addClass('fading');
+      }
+      
+    } else {
+      // If back up near the top, remove sticky + any fade
+      $('.site-header').removeClass('sticky fading');
+    }
+
+    // If scrolling up, remove the fading class
+    if (currentScroll < lastScrollTop) {
+      $('.site-header').removeClass('fading');
+    }
+
+    // Update last scroll position
+    lastScrollTop = currentScroll;
+  });
+}); 
 
 function bindProjectNavArrows(){
   $(".next-project, .prev-project").click(function(evt){
@@ -12,7 +49,7 @@ function bindProjectNavArrows(){
 
 function fadePageOut(targetHref){
   $("#main").fadeOut(200, function(){
-    $( "#main" ).load( targetHref + " #container #main", function(response, status, xhr){
+    $("#main").load(targetHref + " #container #main", function(response, status, xhr){
       bindProjectNavArrows();
       var stateObj = {
         html: $("#main").html()
